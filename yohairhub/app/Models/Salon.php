67ae60c266be725616a7fa5fe69de47a1f029644
GP10DevHTS <jordankatetegirwe@gmail.com>
2024-02-18 08:01:28 +0300

    basic salon model

--- yohairhub/app/Models/Salon.php
+++ yohairhub/app/Models/Salon.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Salon extends Model
 {
     use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name',
+        'slug',
+        'image',
+        'description',
+    ];
+    
 }
--- yohairhub/database/migrations/2024_02_18_044236_create_salons_table.php
+++ yohairhub/database/migrations/2024_02_18_044236_create_salons_table.php
@@ -13,7 +13,12 @@ return new class extends Migration
     {
         Schema::create('salons', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->string('slug')->nullable()->default(null);
+            $table->string('image')->nullable()->default(null);
+            $table->text('description')->nullable()->default(null);
             $table->timestamps();
+            $table->softDeletes();
         });
     }
 
