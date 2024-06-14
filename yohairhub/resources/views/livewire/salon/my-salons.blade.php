f72ba0cc9b2d828638d6b6c8870e37c79e1185b3
Ahairwe Jordan <2000809725@std.kyu.ac.ug>
2024-06-14 16:59:38 +0300

    single salon issue

--- yohairhub/resources/views/livewire/salon/my-salons.blade.php
+++ yohairhub/resources/views/livewire/salon/my-salons.blade.php
@@ -1,16 +1,16 @@
 <div wire:poll>
     {{-- A good traveler has no fixed plans and is not intent upon arriving. --}}
-    @if (auth()->user()->salons->count() > 1)
+    @if (auth()->user()->salons->count() > 0)
         <div class="grid lg:grid-cols-3 sm:grid-cols-1 gap-4 justify-stretch p-5 w-full">
             @foreach (auth()->user()->salons as $salon)
                 @livewire('salon.my-salon-card', ['salon' => $salon], key('salon-card-' . $salon->id))
             @endforeach
         </div>
-    @elseif (auth()->user()->salons->count() == 1)
+    {{-- @elseif (auth()->user()->salons->count() == 1)
         <div
             class="w-ful text-center text-gray-500 dark:text-gray-400 bg-white my-2 p-4 rounded lg:mx-10 text-xl shadow-lg">
             You have one saloon attached to your profile.
-        </div>
+        </div> --}}
     @else
         <div
             class="w-ful text-center text-gray-500 dark:text-gray-400 bg-white my-2 p-4 rounded lg:mx-10 text-xl shadow-lg">
